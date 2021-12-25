7b3164db9caf97cc4c9693e6953316ece29bcb81
Minh <vocongminh3@gmail.com>
2021-12-25 16:50:28 +0700

    template view and get value from database

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('list/', views.viewlist, name="view_list"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 
@@ -8,3 +9,12 @@ def index(request):
     taisan = ["Phone", 'Computer', "Phane"]
     conText = {"name": myName, "taisan": taisan}
     return render(request, "polls/index.html", conText)
+
+
+def viewlist(request):
+    # get_object_or_404 have error if data is none
+    # get_object_or_404 have 2 attribute model  and condition dieu kien example Question , qusetion_text = "Do you have a girlfriend"
+
+    list_question = Question.objects.all()
+    context = {"dsquest": list_question}
+    return render(request, "polls/question.html", context)
