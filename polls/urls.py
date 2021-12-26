c81e6c8ced4f6fce6d080e9d51e0bcdf10035146
Minh <vocongminh3@gmail.com>
2021-12-26 23:58:19 +0700

    select oject from database to template view

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('list/', views.viewlist, name="view_list"),
+    path('detail/<int:question_id>', views.detailView, name="detail"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -14,7 +14,11 @@ def index(request):
 def viewlist(request):
     # get_object_or_404 have error if data is none
     # get_object_or_404 have 2 attribute model  and condition dieu kien example Question , qusetion_text = "Do you have a girlfriend"
-
     list_question = Question.objects.all()
     context = {"dsquest": list_question}
     return render(request, "polls/question.html", context)
+
+
+def detailView(request, question_id):
+    q = Question.objects.get(pk=question_id)
+    return render(request, "polls/detailQuestion.html", {"qs": q})
