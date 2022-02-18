49e37cbfa1c7b51696ef30b152081f77509bf68b
Võ Công Minh <vocongminh3@gmail.com>
2022-02-18 21:45:45 +0700

    nocommand

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('list/', views.viewlist, name="view_list"),
     path('detail/<int:question_id>', views.detailView, name="detail"),
-    path('<int:question_id>', views.vote, name="vote")
-    path('/tailieu', views.tailieu, name="tailieu"),
+    path('<int:question_id>', views.vote, name="vote"),
+    path('/tailieu', views.tailieu, name="tailieu")
 ]
