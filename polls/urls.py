446d3eadc7a309742209e4e4f0602f3ef5708f62
Võ Công Minh <vocongminh3@gmail.com>
2022-04-25 10:26:08 +0700

    fix bug

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('list/', views.viewlist, name="view_list"),
     path('detail/<int:question_id>', views.detailView, name="detail"),
     path('<int:question_id>', views.vote, name="vote"),
-    path('tailieu', views.tailieu, name="tailieu")
+    path('tailieu', views.tailieu, name="tailieu"),
     path('cv', views.cv, name="cv")
 ]
